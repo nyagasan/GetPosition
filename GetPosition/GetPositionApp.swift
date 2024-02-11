155302e9f7e1529b2854dc5ff87a42666e6af2e2
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-02-11 17:12:29 +0900

    Initial Commit

---
+++ GetPosition/GetPositionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetPositionApp.swift
+//  GetPosition
+//
+//  Created by Owner on 2024/02/11.
+//
+
+import SwiftUI
+
+@main
+struct GetPositionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
