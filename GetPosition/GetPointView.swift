f40f62fafe23760277a3a212e8e8c81a34fe68f6
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-02-12 12:02:34 +0900

    Entityで実装してみた

--- GetPosition/GetPointView.swift
+++ GetPosition/GetPointView.swift
@@ -1,29 +1,24 @@
-//
-//  GetPointView.swift
-//  GetPosition
-//
-//  Created by Owner on 2024/02/11.
-//
-
 import SwiftUI
 import RealityKit
 import RealityKitContent
 
 struct GetPointView: View {
-    
-    @State var neptuneEntity: ModelEntity = ModelEntity()
-    
     var body: some View {
-//        Model3D(named: "Neptune", bundle: realityKitContentBundle)
         RealityView { content in
-            if let entity = try? await ModelEntity(named: "Neptune") {
-                let bounds = entity.model!.mesh.bounds.extents
-                entity.components.set(CollisionComponent(shapes: [.generateBox(size: bounds)]))
-                entity.components.set(HoverEffectComponent())
-                entity.components.set(InputTargetComponent())
-                content.add(entity)
+            do {
+                // Anchorオブジェクトの作成
+                let anchor = AnchorEntity()
+                // ModelEntityのロード
+                let loadEntity = try await Entity(named: "Neptune", in: realityKitContentBundle)
+                // AnchorをModelEntityの親に指定
+                loadEntity.setParent(anchor)
+                // いつもの表示するやつ
+                content.add(anchor)
+            } catch {
+                print("Error loading model \(error)")
             }
         }
+
     }
 }
 
