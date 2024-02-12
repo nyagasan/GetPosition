39b8b858e164a82b7a3e98b60b54dee6dda676a4
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-02-12 11:29:30 +0900

    attachmentを削除して読み込み

--- GetPosition/GetPointView.swift
+++ GetPosition/GetPointView.swift
@@ -14,25 +14,16 @@ struct GetPointView: View {
     @State var neptuneEntity: ModelEntity = ModelEntity()
     
     var body: some View {
-        Model3D(named: "Neptune", bundle: realityKitContentBundle)
-        RealityView { content,_ in
-            if let entity = try? await ModelEntity(named: "Neptune", in: realityKitContentBundle) {
+//        Model3D(named: "Neptune", bundle: realityKitContentBundle)
+        RealityView { content in
+            if let entity = try? await ModelEntity(named: "Neptune") {
+                let bounds = entity.model!.mesh.bounds.extents
+                entity.components.set(CollisionComponent(shapes: [.generateBox(size: bounds)]))
+                entity.components.set(HoverEffectComponent())
+                entity.components.set(InputTargetComponent())
                 content.add(entity)
-                entity.scale = SIMD3(0.0015, 0.0015, 0.0015)
-                neptuneEntity = entity
-            }
-        } update: { _, attachments in
-            if let text = attachments.entity(for: "h_t") {
-                text.position = neptuneEntity.position + [0, 0.2, 0]
-                neptuneEntity.addChild(text, preservingWorldTransform: true)
-            }
-        } attachments: {
-            Attachment(id: "h_t") {
-                Text("Hello")
-                    .tag("h_t")
             }
         }
-
     }
 }
 
